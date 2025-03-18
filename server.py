a406e7e7a63d380adc6440f452a57977c32bc549
manmath18 <manmathmungde4@gmail.com>
2025-03-18 13:35:04 +0530

    Added

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 import subprocess
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def run_streamlit():
-    subprocess.run(["streamlit", "run", "app.py", "--server.port", "8000", "--server.headless", "true"])
-    return {"message": "Streamlit app is running"}
+    # Run Streamlit as a subprocess
+    cmd = ["streamlit", "run", "app.py", "--server.port", "8000", "--server.headless", "true", "--browser.serverAddress", "0.0.0.0"]
+    subprocess.Popen(cmd, env=os.environ)
+    return {"message": "Streamlit app is running!"}
